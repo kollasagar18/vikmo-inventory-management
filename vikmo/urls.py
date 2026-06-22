98dfae9867a2f08224cb64047ef166207b47335f
kollasagar18 <kollasagar03@gmail.com>
2026-06-22 17:25:56 +0530

    Added home page

--- vikmo/urls.py
+++ vikmo/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("VIKMO Inventory Management API Running Successfully 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-
-    path(
-        'api/',
-        include('inventory.urls')
-    ),
+    path('api/', include('inventory.urls')),
 ]
