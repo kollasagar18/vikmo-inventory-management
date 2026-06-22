a22b9a5d1805808c7f36802da0a34415e351576c
kollasagar18 <kollasagar03@gmail.com>
2026-06-22 21:03:23 +0530

    Fix Render deployment

--- vikmo/settings.py
+++ vikmo/settings.py
@@ -90,22 +90,23 @@ WSGI_APPLICATION = 'vikmo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': 'vikmo_db',
-    #    'USER': 'postgres',
-    #    'PASSWORD': '1234',
-    #    'HOST': 'localhost',
-    #    'PORT': '5432',
-    #}
-    
-#}
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'vikmo_db',
+            'USER': 'postgres',
+            'PASSWORD': '1234',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
